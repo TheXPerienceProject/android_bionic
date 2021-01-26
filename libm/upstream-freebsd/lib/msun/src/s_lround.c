148f2cd7f8bbb15b3352464383ad759545b7c822
Elliott Hughes <enh@google.com>
2021-01-26 14:17:20 -0800

    Sync libm with upstream FreeBSD.
    Upstream SHA 78599c32efed3247d165302a1fbe8d9203e38974.
    
    Test: treehugger
    Change-Id: Ib103d211315e320df89a6f0bcb30cd8ba67dd603
    Signed-off-by: klozz <klozz@TheXPerienceProject.org>

--- libm/upstream-freebsd/lib/msun/src/s_lround.c
+++ libm/upstream-freebsd/lib/msun/src/s_lround.c
@@ -2,12 +2,12 @@
 
 #ifndef type
 __FBSDID("$FreeBSD$");
-#define type            double
-#define roundit         round
-#define dtype           long
-#define DTYPE_MIN       LONG_MIN
-#define DTYPE_MAX       LONG_MAX
-#define fn              lround
+#define type		double
+#define	roundit		round
+#define dtype		long
+#define	DTYPE_MIN	LONG_MIN
+#define	DTYPE_MAX	LONG_MAX
+#define	fn		lround
 #endif
 
 long lround(double x)
